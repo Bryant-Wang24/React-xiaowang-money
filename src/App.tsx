ea0d6a77d1a7f022d471196dbf3c0a86bf038990
Bryant-Wang24 <485969746@qq.com>
2021-01-27 01:12:39 +0800

    创建 views 目录

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,10 @@ import {
   Route,
   Redirect
 } from 'react-router-dom';
-import Layout from 'components/Layout';
+import Tags from './views/Tags';
+import Money from './views/Money';
+import Statistics from './views/Statistics';
+import NoMatch from './views/NoMatch';
 
 
 function App() {
@@ -28,36 +31,6 @@ function App() {
             </Route>
           </Switch>
     </Router>
-  );
-}
-
-function Tags() {
-  return (
-    <Layout>
-      <h2>标签页面</h2>
-    </Layout>
-  );
-}
-
-function Money() {
-  return (
-    <Layout>
-      <h2>记账页面</h2>
-    </Layout>
-  );
-}
-
-function Statistics() {
-  return (
-    <Layout>
-      <h2>统计页面</h2>
-    </Layout>
-  );
-}
-
-function NoMatch() {
-  return (
-    <div>页面不存在，你个比输错了</div>
   );
 }
 
