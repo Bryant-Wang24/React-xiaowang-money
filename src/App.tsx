e2173a72e2eb0683c6e3eb6c4cfca2166b6d5c19
Bryant-Wang24 <485969746@qq.com>
2021-01-26 21:14:49 +0800

    导航栏布局搞定

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,69 @@
+import React from 'react';
 
+import {
+  HashRouter as Router,
+  Switch,
+  Route,
+  Redirect
+} from 'react-router-dom';
+import styled from 'styled-components';
+import Nav from './components/Nav';
 
-import React from 'react'
+const Wrapper = styled.div`
+  border: 1px solid red;
+  height: 100vh;
+  display: flex;
+  flex-direction: column;
+`;
+const Main = styled.div`
+  flex-grow: 1;
+  overflow: auto;
+  
+  border: 1px solid green;
+`;
+
 
 function App() {
   return (
-    <div>Hello</div>
+    <Router>
+      <Wrapper>
+        <Main>
+          <Switch>
+            <Route path="/tags">
+              <Tags/>
+            </Route>
+            <Route path="/money">
+              <Money/>
+            </Route>
+            <Route path="/statistics">
+              <Statistics/>
+            </Route>
+            <Redirect exact from="/" to="/money"/>
+            <Route path="*">
+              <NoMatch/>
+            </Route>
+          </Switch>
+        </Main>
+        <Nav/>
+      </Wrapper>
+    </Router>
+  );
+}
+
+function Tags() {
+  return <h2>标签页面</h2>;
+}
+
+function Money() {
+  return <h2>记账页面</h2>;
+}
+
+function Statistics() {
+  return <h2>统计页面</h2>;
+}
+function NoMatch() {
+  return (
+    <div>页面不存在，你个比输错了</div>
   );
 }
 
