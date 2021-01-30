dcd8f8dc09a877eccb5bb15873099734fcd41539
Bryant-Wang24 <485969746@qq.com>
2021-01-30 20:58:52 +0800

    抽离出Button、Input组件，完成Tag.tsx 的样式

--- src/views/Tag.tsx
+++ src/views/Tag.tsx
@@ -1,16 +1,54 @@
 import React from 'react';
-import { useParams } from 'react-router-dom';
+import {useParams} from 'react-router-dom';
 import {useTags} from '../useTags';
+import Layout from '../components/Layout';
+import styled from 'styled-components';
+import Icon from '../components/Icon';
+import {Button} from '../components/Button';
+import {Center} from '../components/Center';
+import {Space} from '../components/Space';
+import {Input} from '../components/Input';
 
 type Params = {
-  id:string
+  id: string
 }
-const Tag:React.FC = (props)=>{
-  const  {findTag} = useTags()
-  let {id} = useParams<Params>()
-  const tag = findTag(parseInt(id))
-  return(
-    <div>{tag.name}</div>
-  )
-}
-export {Tag}
+const Topbar = styled.header`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  line-height: 20px;
+  padding: 14px;
+  background: white;
+`;
+const InputWrapper = styled.div`
+  background:white;
+  padding: 14px 16px;
+  margin-top: 8px;
+`
+
+const Tag: React.FC = (props) => {
+  const {findTag} = useTags();
+  let {id} = useParams<Params>();
+  const tag = findTag(parseInt(id));
+  return (
+    <Layout>
+      <Topbar>
+        <Icon name="left"/>
+        <span>编辑标签</span>
+        <Icon/>
+      </Topbar>
+      <div>
+        <InputWrapper>
+          <Input label="标签名" type="text" placeholder="标签名" value={tag.name}/>
+        </InputWrapper>
+      </div>
+      <Center>
+        <Space/>
+        <Space/>
+        <Space/>
+        <Button>删除标签</Button>
+      </Center>
+    </Layout>
+  );
+};
+export {Tag};
--- src/views/Tags.tsx
+++ src/views/Tags.tsx
@@ -4,6 +4,9 @@ import {useTags} from '../useTags';
 import Icon from '../components/Icon';
 import styled from 'styled-components';
 import {Link} from 'react-router-dom';
+import {Button} from '../components/Button';
+import {Center} from '../components/Center';
+import {Space} from '../components/Space';
 
 const TagList = styled.ol`
   font-size: 16px;
@@ -22,23 +25,9 @@ const TagList = styled.ol`
     }
   }
 `;
-const Button = styled.button`
-  font-size: 18px;
-  border: none;
-  padding: 8px 12px;
-  background: #f60;
-  border-radius: 4px;
-  color: white;
-`;
-const Center = styled.div`
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  flex-direction: column;
-`;
-const Space = styled.div`
-  height: 16px;
-`;
+
+
+
 
 function Tags() {
   const {tags} = useTags();
